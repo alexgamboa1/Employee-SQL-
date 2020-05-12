ce073d73327d92756b99aa7d63615707bddf9c5f
alexgamboa1 <ag.gamboa94@gmail.com>
2020-05-12 13:02:17 -0700

    Commit dept_emp & titles schema

--- schema.sql
+++ schema.sql
@@ -20,3 +20,30 @@ CREATE TABLE departments (
 
 SELECT * 
 FROM departments
+
+CREATE TABLE dept_manager(
+	dept_no 	VARCHAR(4) 		NOT NULL,
+	emp_no 		INT 			NOT NULL, 
+	from_date   DATE            NOT NULL,
+   	to_date      DATE            NOT NULL,
+   	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no), 
+	PRIMARY KEY (emp_no, dept_no)
+); 
+
+SELECT *
+FROM dept_manager
+
+CREATE TABLE dept_emp(
+	emp_no		INT			NOT NULL,
+	dept_no		VARCHAR(4)	NOT NULL, 
+	from_date 	DATE 		NOT NULL, 
+	to_date 	DATE 		NOT NULL, 
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no), 
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+
+
+
