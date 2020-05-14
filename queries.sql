fc22124994efacdbf9e0667d3686596533867070
alexgamboa1 <ag.gamboa94@gmail.com>
2020-05-13 22:46:06 -0700

    Commit employees hired in 86 and list of managers

--- queries.sql
+++ queries.sql
@@ -12,7 +12,29 @@ FROM employees AS emp
 ORDER BY emp.emp_no;
 
 
+-- List employees who were hired in 1986.
+select * from employees;
 
+SELECT * FROM employees WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+--List the manager of each department with the following information: 
+-- department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
+select * from dept_manager;
+select * from departments;
+select * from departments;
+
+SELECT dm.dept_no,
+	d.dept_name, 
+	dm.emp_no,
+	emp.last_name,
+	emp.first_name, 
+	dm.from_date, 
+	dm.to_date
+FROM dept_manager AS dm
+INNER JOIN departments as d
+	ON (dm.dept_no = d.dept_no)
+INNER JOIN employees as emp
+	ON (dm.emp_no = emp.emp_no);
 
 
 
